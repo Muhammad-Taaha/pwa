8fe4f82b6d38a819e05e0dc667317d07dfe26d93
muhammad-taaha <taha74697@gmail.com>
2026-02-08 17:33:09 +0500

    second pus

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "voice-cache-v1";
+
+const FILES_TO_CACHE = ["./", "./index.html", "./voice.mp3", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    }),
+  );
+});
